4b6d412980d765a93ebfe83a2845168002f1846a
Guilherme Cezarino Felipe <124943256+GuiCezaF@users.noreply.github.com>
2023-07-04 13:11:50 -0300

    chore: add sequelize and schedule

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TweetsModule } from './tweets/tweets.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { join } from 'path';
+import { ScheduleModule } from '@nestjs/schedule';
+
+@Module({
+  imports: [
+    ScheduleModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      host: join(__dirname, 'database.sqlite'),
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    TweetsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
