c0ab336c1a5b869208461e6f9465a1772c467155
Guilherme Cezarino Felipe <124943256+GuiCezaF@users.noreply.github.com>
2023-07-10 18:15:08 -0300

    feat: add tweets count service

--- src/tweets/tweet-count/tweet-count.service.ts
+++ src/tweets/tweet-count/tweet-count.service.ts
@@ -1,17 +1,38 @@
-import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
-import { Tweet } from '../entities/tweet.entity';
+import { Tweet } from './../entities/tweet.entity';
+import { Inject, Injectable } from '@nestjs/common';
 import { Interval } from '@nestjs/schedule';
+import { Cache } from 'cache-manager';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 
 @Injectable()
 export class TweetCountService {
+  private limit = 10;
   constructor(
     @InjectModel(Tweet)
     private tweetModel: typeof Tweet,
+    @Inject(CACHE_MANAGER)
+    private cacheManager: Cache,
   ) {}
 
   @Interval(5000)
   async countTweets() {
-    console.log('executou');
+    console.log('procurando tweets');
+    let offset = await this.cacheManager.get<number>('tweet-offset');
+    offset = offset === undefined ? 0 : offset;
+
+    console.log(`offsets: ${offset}`);
+
+    const tweets = await this.tweetModel.findAll({
+      offset,
+      limit: this.limit,
+    });
+
+    console.log(`${tweets.length} encontrados`);
+
+    if (tweets.length === this.limit) {
+      this.cacheManager.set('tweet-offset', offset + this.limit);
+      console.log(`achou + ${this.limit} tweets`);
+    }
   }
 }
--- src/tweets/tweets.module.ts
+++ src/tweets/tweets.module.ts
@@ -4,9 +4,10 @@ import { TweetsController } from './tweets.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Tweet } from './entities/tweet.entity';
 import { TweetCountService } from './tweet-count/tweet-count.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Tweet])],
+  imports: [CacheModule.register(), SequelizeModule.forFeature([Tweet])],
   controllers: [TweetsController],
   providers: [TweetsService, TweetCountService],
 })
